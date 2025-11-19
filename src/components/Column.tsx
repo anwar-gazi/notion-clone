b88a34b4e5a74f99d5a177deb3f0d6517d7a9c3e
Minhajul Anwar <minhaj.me.bd@gmail.com>
2025-11-19 16:25:04 +0600

    feat(dnd): make column droppable and expose columnId via droppable data

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,24 +1,32 @@
 "use client";
 import { useDroppable } from "@dnd-kit/core";
 import TaskCard from "./TaskCard";
-import { TaskData } from "@/types/data";
+import { ColumnDTO, TaskDTO } from "@/types/data";
+import { useTasksInColumn } from "./BoardContext";
 
-export default function Column({ column }: { column: { id: string, name: string, tasks: TaskData[] } }) {
+/**
+ * TODO optimize the number of times it is rendered
+ * @param param0 
+ * @returns 
+ */
+export default function Column({ column }: { column: ColumnDTO }) {
+  const tasks = useTasksInColumn(column.id);
   const { setNodeRef, isOver } = useDroppable({
     id: column.id,
     data: { type: 'column', columnId: column.id }
   });
+  
   return (
     <div className="bg-white rounded-2xl shadow p-4">
       <header className="font-semibold mb-2 flex items-center justify-between">
-        <span>{column.name}</span><span className="text-sm text-gray-400">{column.tasks.length}</span>
+        <span>{column.name}</span><span className="text-sm text-gray-400">{column.taskIds.length}</span>
       </header>
       <div
         ref={setNodeRef}
         className={`space-y-3 min-h-[40px] p-1 rounded ${isOver ? "ring-2 ring-black/40" : ""
           }`}
       >
-        {column.tasks.map((t: any) => (<TaskCard key={t.id} task={t} />))}
+        {tasks.map((task: TaskDTO) => (<TaskCard key={task.id} task={task} />))}
       </div>
     </div>
   );
